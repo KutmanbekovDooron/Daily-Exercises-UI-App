7b94012f970f1d66371f3c5e6fb68f387ae25d37
KutmanbekovDooron <kutmanbekovdooron69@gmail.com>
2023-08-03 01:37:10 +0600

    finished

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -2,11 +2,46 @@ import 'package:daily_exercises/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      height: 80,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          BottomNavItem(
+            title: 'Today',
+            icon: 'calendar',
+            press: () {},
+          ),
+          BottomNavItem(
+            title: 'All Exercises',
+            icon: 'gym',
+            press: () {},
+            isActive: true,
+          ),
+          BottomNavItem(
+            title: 'Settings',
+            icon: 'Settings',
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class BottomNavItem extends StatelessWidget {
   final String title;
   final String icon;
   final VoidCallback press;
   final bool isActive;
+
   const BottomNavItem({
     required this.title,
     required this.icon,
@@ -18,12 +53,18 @@ class BottomNavItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap:press,
+      onTap: press,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          SvgPicture.asset("assets/icons/$icon.svg", color: isActive ? kActiveIconColor : kTextColor,),
-          Text(title, style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),)
+          SvgPicture.asset(
+            "assets/icons/$icon.svg",
+            color: isActive ? kActiveIconColor : kTextColor,
+          ),
+          Text(
+            title,
+            style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),
+          )
         ],
       ),
     );
